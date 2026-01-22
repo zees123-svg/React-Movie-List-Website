0142e455943e404fc9e077bc841344ebe68ad819
zees123-svg <versatilezeeshan1708@gmail.com>
2026-01-22 11:37:03 +0500

    Add initial files

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-movie-list-website
-This is a react base movie list website in which you can see the list of movies and also added some movies into your favourites movie list page.
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "d6f4258b7dab590d99b96dce43d8ff88";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
